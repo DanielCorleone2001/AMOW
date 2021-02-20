55742213d410793ad24cde69c16a55b78b2432fb
DanielCorleone2001 <gzqin2001@gmail.com>
2021-02-20 10:52:16 +0800

    完成角色查询功能

--- src/main/java/com/daniel/vo/request/RolePageReqVO.java
+++ src/main/java/com/daniel/vo/request/RolePageReqVO.java
@@ -18,4 +18,19 @@ public class RolePageReqVO {
 
     @ApiModelProperty(value = "分页数量")
     private int pageSize = 10;
+
+    @ApiModelProperty(value = "角色ID")
+    private String roleId;
+
+    @ApiModelProperty(value = "角色名称")
+    private String roleName;
+
+    @ApiModelProperty(value = "开始时间")
+    private String startTime;
+
+    @ApiModelProperty(value = "结束时间")
+    private String endTime;
+
+    @ApiModelProperty(value = "状态")
+    private Integer status;
 }
